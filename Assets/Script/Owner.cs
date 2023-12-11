a23472dce61354473f125ed6823febfc3a6a254a
chriswastyping <76431488+chriswastyping@users.noreply.github.com>
2023-12-11 21:14:26 +0000

    Fixes on terrain
    Too much happening

--- Assets/Script/Owner.cs
+++ Assets/Script/Owner.cs
@@ -1,24 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Owner : MonoBehaviour
 {
-    public float speed = 3.0f;
-    private Rigidbody ownerRb;
-    private GameObject player;
+    //public float speed = 3.0f;
+    //private Rigidbody ownerRb;
+    //private GameObject player;
+
+    // Nav Mesh
+
+    public Transform player;
+    private NavMeshAgent agent;
 
     void Start()
     {
-        ownerRb = GetComponent<Rigidbody>();
-        player = GameObject.Find("Fred");
+        //ownerRb = GetComponent<Rigidbody>();
+        //player = GameObject.Find("Fred");
+
+        agent = GetComponent<NavMeshAgent>();
     }
 
     void FixedUpdate()
     {
        
-            Vector3 lookDirection = (player.transform.position - transform.position).normalized;
-            ownerRb.velocity = lookDirection * speed;
+           // Vector3 lookDirection = (player.transform.position - transform.position).normalized;
+           // ownerRb.velocity = lookDirection * speed;
+
+            agent.destination = player.position;
 
     }
 }
